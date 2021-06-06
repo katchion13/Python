65642acb7440d431fd44c33a53c15d1a15804ed4
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-06-06 21:34:22 +0900

    Update flask_sample.py

--- flask_sample.py
+++ flask_sample.py
@@ -4,4 +4,4 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
-app.run(port=12345, debug=True)
+app.run(port=8000, debug=True)
